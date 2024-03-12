f29e486b08c180188776f4046d50ce410f1fab55
nhthanhdn95 <nhthanhdn95@gmail.com>
2024-03-12 13:52:51 +1030

    Add User routes and Place routes

---
+++ BE/controllers/places-controller.js
@@ -0,0 +1,83 @@
+const { v4: uuidv4 } = require("uuid");
+
+const HttpError = require("../models/http-error");
+
+let DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    location: {
+      lat: 40.7484,
+      lng: -73.9857,
+    },
+    address: "20 W 34th St., New York, NY 10001",
+    creator: "u1",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id.", 404); // dont have to return, because 'throw' cancles the function execution
+  }
+
+  res.json({ place }); // => { place } => { place: place }
+};
+
+const getPlacesByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const places = DUMMY_PLACES.filter((p) => p.creator === userId);
+
+  if (!places || places.length === 0) {
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404)
+    ); // must use  return, because 'next' does not cancle the function execution
+  }
+
+  res.json({ places });
+};
+
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  // const title = req.body.title;
+  const createdPlace = {
+    id: uuidv4(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+
+  DUMMY_PLACES.push(createdPlace);
+  res.status(201).json({ place: createdPlace });
+};
+
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+  const existingPlace = DUMMY_PLACES.find((p) => p.id === placeId);
+  const updatedPlace = { ...existingPlace };
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({ place: updatedPlace });
+};
+
+const deletePlace = (req, res, next) => {
+  const placeId = req.params.pid;
+  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
+  res.status(200).json({ message: "Deleted place." });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlacesByUserId = getPlacesByUserId;
+exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
--- BE/routes/places-routes.js
+++ BE/routes/places-routes.js
@@ -1,44 +1,17 @@
 const express = require("express");
 
-const HttpError = require("../models/http-error");
+const placesControllers = require("../controllers/places-controller");
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    location: {
-      lat: 40.7484,
-      lng: -73.9857,
-    },
-    address: "20 W 34th St., New York, NY 10001",
-    creator: "u1",
-  },
-];
+router.get("/:pid", placesControllers.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+router.get("/user/:uid", placesControllers.getPlacesByUserId);
 
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided id.", 404); // dont have to return, because 'throw' cancles the function execution
-  }
+router.post("/", placesControllers.createPlace);
 
-  res.json({ place }); // => { place } => { place: place }
-});
+router.patch("/:pid", placesControllers.updatePlace);
 
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => p.creator === userId);
+router.delete("/:pid", placesControllers.deletePlace);
 
-  if (!place) {
-    return next(
-      new HttpError("Could not find a place for the provided user id.", 404)
-    ); // must use  return, because 'next' does not cancle the function execution
-  }
-
-  res.json({ place });
-});
 module.exports = router;
