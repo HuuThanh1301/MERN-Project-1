6b0a76d6fb99d6491006355ca00e13866a156b61
nhthanhdn95 <nhthanhdn95@gmail.com>
2024-03-17 21:03:48 +1030

    Send Post Resquest to Backend & Handle CORS Errrors & Sending Login Request

--- BE/app.js
+++ BE/app.js
@@ -10,6 +10,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Original, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/places", placesRoutes);
 
 app.use("/api/users", usersRoutes);
@@ -28,7 +38,9 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect("mongodb+srv://huuthanh:13011995@cluster0.1rkl52p.mongodb.net/places?retryWrites=true&w=majority")
+  .connect(
+    "mongodb+srv://huuthanh:13011995@cluster0.1rkl52p.mongodb.net/mern?retryWrites=true&w=majority"
+  )
   .then(() => {
     app.listen(5000);
   })
